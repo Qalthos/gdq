1732403651c5f35457cc5bbacdadd4dff97671ce
Nathaniel Case <Qalthos@gmail.com>
2019-01-07 11:31:31 -0500

    Attempt to update esa blind

--- esaboard.py
+++ esaboard.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
-from datetime import datetime
 import shutil
 import sys
 
 from bs4 import BeautifulSoup
 from dateutil import parser
-import pytz
 import requests
 
-from utils import read_incentives, display_run
+from utils import NOW, Run, read_incentives, display_run
 
 BID_TRACKER = 'https://donations.esamarathon.com/bids/2018s'
 SCHEDULE = 'https://esamarathon.com/schedule'
@@ -26,37 +24,32 @@ def read_schedule(schedule_url, stream_index=1):
     schedule = header.find_next('table').tbody
     run_starts = schedule.find_all('time', class_='time-only')
 
-    now = datetime.now(pytz.utc)
     for index, day_row in enumerate(run_starts):
         time = parser.parse(day_row.attrs['datetime'])
-        if time > now:
-            return [parse_run(td.parent.parent, now) for td in run_starts[index - 1:]]
+        if time > NOW:
+            return [parse_run(td.parent.parent) for td in run_starts[index - 1:]]
 
     print("Nothing running right now ):")
     return []
 
 
-def parse_run(row, now):
+def parse_run(row):
     """Parse run metadata from schedule row."""
 
-    run = dict(delta='  NOW  ')
-
     time = parser.parse(row.td.time.attrs['datetime'])
-    if time > now:
-        delta = time - now
-        run['delta'] = '{0}:{1[0]:02d}:{1[1]:02d}'.format(delta.days, divmod(delta.seconds // 60, 60))
-
-    run['game'] = row.contents[1].p.a.string
-    run['estimate'] = row.contents[2].string
     try:
-        run['runner'] = row.contents[3].p.a.string
-        run['platform'] = row.contents[4].string.strip()
-        run['runtype'] = row.contents[5].string.strip()
+        runner = row.contents[3].p.a.string
+        platform = row.contents[4].string.strip()
+        runtype = row.contents[5].string.strip()
     except AttributeError:
         # Assume offline block
-        run['runner'] = ''
-        run['platform'] = ''
-        run['runtype'] = ''
+        runner = ''
+        platform = ''
+        runtype = ''
+    run = Run(
+        game=row.contents[1].p.a.string, platform=platform, runtype=runtype,
+        runner=runner, start=time, str_estimate=row.contents[2].string,
+    )
 
     return run
 
