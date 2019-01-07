4db367b8828c3b598c374c6b70ba25151e98b4dc
Nathaniel Case <Qalthos@gmail.com>
2019-01-07 11:25:39 -0500

    Move run logic to Run dataclass

--- gdqboard.py
+++ gdqboard.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
-from datetime import datetime
-import re
 import shutil
 
 from bs4 import BeautifulSoup
 from dateutil import parser
-import pytz
 import requests
 
-from utils import read_incentives, display_run
+from utils import NOW, Run, read_incentives, display_run
 
 BID_TRACKER = 'https://gamesdonequick.com/tracker/bids/agdq2019'
 SCHEDULE = 'https://gamesdonequick.com/schedule'
@@ -25,35 +22,32 @@ def read_schedule(schedule_url, stream_index=1):
     schedule = soup.find('table', id='runTable').tbody
     run_starts = schedule.find_all('td', class_='start-time')
 
-    now = datetime.now(pytz.utc)
     for index, day_row in enumerate(run_starts):
         time = parser.parse(day_row.text)
-        if time > now:
+        if time > NOW:
             # If we havent started yet, index should still be 0
             start = max(index - 1, 0)
-            return [parse_run(td.parent, now) for td in run_starts[start:]]
+            return [parse_run(td.parent) for td in run_starts[start:]]
 
     print("Nothing running right now ):")
     return []
 
 
-def parse_run(row, now):
+def parse_run(row):
     """Parse run metadata from schedule row."""
 
     row2 = row.find_next_sibling()
     if not row2:
         return None
-    run = dict(delta='  NOW  ')
 
     time = parser.parse(row.contents[1].string)
-    if time > now:
-        delta = time - now
-        run['delta'] = '{0}:{1[0]:02d}:{1[1]:02d}'.format(delta.days, divmod(delta.seconds // 60, 60))
 
-    run['game'] = row.contents[3].string
-    run['runner'] = row.contents[5].string
-    run['estimate'] = ''.join(row2.contents[1].stripped_strings)
-    run['runtype'], _, run['platform'] = row2.contents[3].string.rpartition(' — ')
+    runtype, _, platform = row2.contents[3].string.rpartition(' — ')
+    estimate = ''.join(row2.contents[1].stripped_strings)
+    run = Run(
+        game=row.contents[3].string, platform=platform, runtype=runtype,
+        runner=row.contents[5].string, start=time, str_estimate=estimate,
+    )
 
     return run
 
--- utils.py
+++ utils.py
@@ -1,7 +1,51 @@
+from dataclasses import dataclass
+from datetime import datetime, timedelta
 import re
 
 from bs4 import BeautifulSoup
+import pytz
 import requests
+
+
+NOW = datetime.now(pytz.utc)
+PREFIX = ' ' * 7
+
+
+@dataclass
+class Run:
+    game: str
+    platform: str
+    runtype: str
+    runner: str
+
+    start: datetime
+    str_estimate: str
+
+    @property
+    def delta(self):
+        if self.start < NOW:
+            return '  NOW  '
+        delta = self.start - NOW
+        hours, minutes = divmod(delta.seconds // 60, 60)
+        return f'{delta.days}:{hours:02d}:{minutes:02d}'
+
+    @property
+    def raw_estimate(self):
+        hours, minutes, seconds = self.str_estimate.split(':')
+        estimate = timedelta(hours=int(hours), minutes=int(minutes), seconds=int(seconds))
+        if self.start < NOW:
+            estimate = self.start + estimate - NOW
+        return estimate
+
+    @property
+    def estimate(self):
+        hours, minutes = divmod(self.raw_estimate.seconds, 3600)
+        minutes = minutes // 60
+        return f'+{hours}:{minutes:02d}'
+
+    @property
+    def game_desc(self):
+        return f'{self.game} ({self.platform})'
 
 
 def show_progress(percent, width=72):
@@ -57,35 +101,30 @@ def read_incentives(incentive_url):
 
 
 def display_run(run, incentive_dict, width=80):
-    prefix = ' ' * 7
-    width -= len(prefix) + 1
+    width -= len(PREFIX) + 1
 
-    runner = '│' + run['runner'] + '│'
-    print('{0}┼{1}┬{2}┐'.format('─' * 7, '─' * (width - len(runner)), '─' * len(run['runner'])))
+    runner = '│' + run.runner + '│'
+    print('{0}┼{1}┬{2}┐'.format('─' * 7, '─' * (width - len(runner)), '─' * len(run.runner)))
 
-    game = '{game} ({platform})'.format(**run)
     line_one = "{0}│{1:<49s} {2: >" + str(width - 50) + "s}"
-    print(line_one.format(run['delta'], game, runner))
+    print(line_one.format(run.delta, run.game_desc, runner))
 
-    estimate = '+%s' % run['estimate'].rsplit(':', 1)[0]
-    if run['delta'] == '  NOW  ':
-        estimate = ''
-    line_two = "{0: >7s}│{1:<" + str(width - len(run['runner']) - 2) + "}└{2}┘"
-    print(line_two.format(estimate, run['runtype'], '─' * len(run['runner'])))
+    line_two = "{0: >7s}│{1:<" + str(width - len(run.runner) - 2) + "}└{2}┘"
+    print(line_two.format(run.estimate, run.runtype, '─' * len(run.runner)))
 
     # Handle incentives
-    for incentive in incentive_dict.get(run['game'], []):
+    for incentive in incentive_dict.get(run.game, []):
         if 'total' in incentive:
             percent = incentive['current'] / incentive['total'] * 100
             progress_bar = show_progress(percent, width - 42)
             total = '${0:,.0f}'.format(incentive['total'])
             print('{3}├┬{0:<31s} {1}{2: >7s}'.format(
-                incentive['short_desc'], progress_bar, total, prefix
+                incentive['short_desc'], progress_bar, total, PREFIX
             ))
-            print('{1}│└▶{0}'.format(incentive['description'], prefix))
+            print('{1}│└▶{0}'.format(incentive['description'], PREFIX))
         elif 'options' in incentive:
             print('{2}├┬{0:<32s} {1}'.format(
-                incentive['short_desc'], incentive['description'], prefix
+                incentive['short_desc'], incentive['description'], PREFIX
             ))
             for index, option in enumerate(incentive['options']):
                 try:
@@ -95,11 +134,11 @@ def display_run(run, incentive_dict, width=80):
                 progress_bar = show_progress(percent, width - 42)
                 total = '${0:,.0f}'.format(option['total'])
                 if index == len(incentive['options']) - 1:
-                    print('{3}│└▶{0:<30s} {1}{2: >7s}'.format(option['choice'], progress_bar, total, prefix))
+                    print('{3}│└▶{0:<30s} {1}{2: >7s}'.format(option['choice'], progress_bar, total, PREFIX))
                 else:
-                    print('{3}│├▶{0:<30s} {1}{2: >7s}'.format(option['choice'], progress_bar, total, prefix))
+                    print('{3}│├▶{0:<30s} {1}{2: >7s}'.format(option['choice'], progress_bar, total, PREFIX))
                 if option['description']:
-                    print('{1}│  └▶{0}'.format(option['description'], prefix))
+                    print('{1}│  └▶{0}'.format(option['description'], PREFIX))
 
 
 if __name__ == '__main__':
