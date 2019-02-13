76a981de2dfcc790de6689d8da4ac5dcc8fcc5e5
Nathaniel Case <Qalthos@gmail.com>
2019-02-13 14:29:16 -0500

    Use new display methods
    single_board.py for events that haven't been updated

--- board.py
+++ board.py
@@ -1,26 +1,19 @@
 #!/usr/bin/env python
-import argparse
 import shutil
-from display import display_run, display_milestone
-from scrapers.esa import BID_TRACKER, RECORDS, read_schedule, read_total
+from display import display_runs, display_milestone
+from scrapers.esa import BID_TRACKER, RECORDS, STREAMS, read_schedules, read_total
 from utils import read_incentives
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--stream_index', help="Stream index to follow", type=int, default=1)
-    args = parser.parse_args()
-
     width, height = shutil.get_terminal_size()
-    show_count = height // 3 + 1
 
-    runs = read_schedule(args.stream_index)
-    incentives = read_incentives(BID_TRACKER, args.stream_index)
+    incentives = {}
+    for stream in STREAMS:
+        incentives.update(read_incentives(BID_TRACKER, stream))
 
-    total = read_total()
-    display_milestone(total, RECORDS, width)
-    for run in runs[:show_count]:
-        display_run(run, incentives, width)
+    display_milestone(read_total(), RECORDS, width)
+    display_runs(read_schedules(), incentives, width, height - 1)
 
 
 if __name__ == '__main__':
---
+++ single_board.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import argparse
+import shutil
+from display import display_run, display_milestone
+from scrapers.esa import BID_TRACKER, RECORDS, read_schedule, read_total
+from utils import read_incentives
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--stream_index', help="Stream index to follow", type=int, default=1)
+    args = parser.parse_args()
+
+    width, height = shutil.get_terminal_size()
+    show_count = height // 3 + 1
+
+    runs = read_schedule(args.stream_index)
+    incentives = read_incentives(BID_TRACKER, args.stream_index)
+
+    display_milestone(read_total(), RECORDS, width)
+    for run in runs[:show_count]:
+        display_run(run, incentives, width)
+
+
+if __name__ == '__main__':
+    main()
