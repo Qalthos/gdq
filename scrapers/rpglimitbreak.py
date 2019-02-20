7b364609bb02f63d405a2676227318a0cb792700
Nathaniel Case <Qalthos@gmail.com>
2019-02-20 18:30:43 -0500

    Port RPGLimitBreak
    Hooray, we finally get platforms

--- scrapers/rpglimitbreak.py
+++ scrapers/rpglimitbreak.py
@@ -1,8 +1,10 @@
+from datetime import datetime
+
 from bs4 import BeautifulSoup
-from dateutil import parser
+import pytz
 
 from scrapers import MarathonBase
-from utils import NOW, Run
+from utils import Run
 
 EVENT = 'rpglb2018'
 URL = 'https://www.rpglimitbreak.com/tracker'
@@ -16,53 +18,29 @@ RECORDS = sorted([
 
 class RPGLimitBreak(MarathonBase):
     index_url = f'{URL}/index/{EVENT}'
-    schedule_url = f'{URL}/runs/{EVENT}'
     incentive_url = f'{URL}/bids/{EVENT}'
+    event_id = 'rpglb'
+    stream_ids = ['2018']
 
     def read_total(self, streams):
         source = self.session.get(self.index_url).text
         soup = BeautifulSoup(source, 'html.parser')
 
         total = soup.find('h2').small.string
-        total = total.split()[2].split(' (')[0].replace(',', '')[1:]
+        total = float(total.split()[2].split(' (')[0].replace(',', '')[1:])
 
-        return float(total)
+        return total
 
-    def read_schedules(self):
-        return [self.read_schedule(1)]
+    @classmethod
+    def parse_data(cls, keys, schedule, timezone='UTC'):
+        for run in schedule:
+            rundata = dict(zip(keys, run['data']))
 
-    def read_schedule(self, stream_index):
-        if stream_index != 1:
-            print("Index {} is not valid for this steam".format(stream_index))
-            return []
-
-        source = self.session.get(self.schedule_url).text
-        soup = BeautifulSoup(source, 'html.parser')
-
-        schedule = soup.find('table')
-        run_starts = schedule.find_all('span', class_='datetime')[::2]
-
-        for index, row in enumerate(run_starts):
-            time = parser.parse(row.text)
-            if time > NOW:
-                # If we havent started yet, index should still be 0
-                start = max(index - 1, 0)
-                return [_parse_run(span.parent.parent) for span in run_starts[start:]]
-
-        print("Nothing running right now ):")
-        return []
-
-
-def _parse_run(row):
-    """Parse run metadata from schedule row."""
-
-    start = parser.parse(row.contents[7].span.string)
-    end = parser.parse(row.contents[9].span.string)
-
-    estimate = (end - start).total_seconds()
-    run = Run(
-        game=row.contents[1].a.string, runner=row.contents[3].string.strip(),
-        platform='', category='', start=start, estimate=estimate,
-    )
-
-    return run
+            yield Run(
+                game=rundata['Game'],
+                platform=rundata['System'],
+                category=rundata['Category'],
+                runner=cls.strip_md(rundata['Runner(s)']),
+                start=datetime.fromtimestamp(run['scheduled_t'], tz=pytz.timezone(timezone)),
+                estimate=run['length_t'],
+            )
