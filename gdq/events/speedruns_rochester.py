8e8c617d86ad6ce1bef3d968720667731109b7c7
Nathaniel Case <Qalthos@gmail.com>
2019-06-14 13:04:41 -0400

    Allow dead (sleep) items to show up.

--- gdq/events/speedruns_rochester.py
+++ gdq/events/speedruns_rochester.py
@@ -13,14 +13,11 @@ def parse_data(keys, schedule, timezone="UTC") -> Generator:
     for run in schedule:
         run_data = dict(zip(keys, run["data"]))
 
-        if run_data["Runner"] is None:
-            continue
-
         yield Run(
             game=run_data["Game"],
-            platform=run_data["Console"],
-            category=run_data["Category"],
-            runner=run_data["Runner"],
+            platform=run_data["Console"] or "",
+            category=run_data["Category"] or "",
+            runner=run_data["Runner"] or "",
             start=datetime.fromtimestamp(run["scheduled_t"], tz=tz.gettz(timezone)),
             estimate=run["length_t"],
         )
