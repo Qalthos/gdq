08c970fa248e24ffb977ebd890c7b09b789cabec
Nathaniel Case <Qalthos@gmail.com>
2019-06-14 12:53:18 -0400

    Add setup.py for entry_points

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="gdq-cli",
+    version=0.9,
+    packages=find_packages(exclude=['tests']),
+    entry_points={
+        'console_scripts': [
+            'gdq=gdq.board:main',
+        ],
+    },
+)
