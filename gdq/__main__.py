10dcd1c8d382d17835fe9fac53bbb18d3f40197a
Nathaniel Case <Qalthos@gmail.com>
2020-07-06 11:01:46 -0400

    Hook up list to __main__

--- gdq/__main__.py
+++ gdq/__main__.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 import argparse
-from pathlib import Path
 import sys
+from datetime import datetime, timedelta
+from pathlib import Path
+from typing import Dict, Optional, Tuple
 
 import toml
 import xdg
+from dateutil import tz
 
-from gdq.events import MarathonBase
 from gdq import runners, utils
+from gdq.events import MarathonBase
 
 
 def refresh_event(marathon: MarathonBase, args: argparse.Namespace) -> bool:
@@ -25,12 +28,34 @@ def refresh_event(marathon: MarathonBase, args: argparse.Namespace) -> bool:
     return True
 
 
+def list_events(config: dict) -> None:
+    event_times: Dict[str, Tuple[datetime, Optional[datetime]]] = {}
+    for name, marathon_config in config.items():
+        runner = runners.get_runner(marathon_config)
+        event_times[name] = runner.get_times(marathon_config)
+
+    for name, (start, end) in sorted(event_times.items(), key=lambda x: x[1]):
+        if end is None:
+            end = start + timedelta(days=7)
+
+        if utils.now < start:
+            print(f"{name} will start in {start - utils.now}")
+        elif end < utils.now:
+            print(f"{name} (probably) finished on {end.astimezone(tz.gettz())}")
+        else:
+            print(f"{name} is (probably) ongoing")
+
+
 def main():
     with open(Path(xdg.XDG_CONFIG_HOME) / "gdq" / "config.toml") as toml_file:
         config = toml.load(toml_file)
 
     base_parser = runners.get_base_parser()
     base_args, _extra = base_parser.parse_known_args()
+
+    if base_args.list:
+        list_events(config)
+        sys.exit(0)
 
     event_config = config.get(base_args.stream_name)
     if event_config is None:
