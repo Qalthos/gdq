37ae2b73994448190cd4a8ac66fd9bc694ae32cf
Nathaniel Case <Qalthos@gmail.com>
2020-08-15 10:25:24 -0400

    Fix BSG to be more GDQ-like

--- gdq/events/bsg.py
+++ gdq/events/bsg.py
@@ -1,15 +1,7 @@
-from typing import Any
-
 from gdq.events.gdq import GDQTracker
 from gdq.parsers import bsg
 
 
 class BSGTracker(GDQTracker):
-    event_name: str
-
-    def __init__(self, event_name: str, *args: Any, **kwargs: Any):
-        self.event_name = event_name
-        super().__init__(*args, **kwargs)
-
     def read_schedules(self) -> None:
-        self.schedules = [bsg.get_runs(self.url, self.event_name)]
+        self.schedules = [bsg.get_runs(self.url, self.current_event.short_name.lower())]
--- gdq/runners/bsg.py
+++ gdq/runners/bsg.py
@@ -1,6 +1,5 @@
-import argparse
 from datetime import datetime
-from typing import List, Optional, Tuple
+from typing import Optional, Tuple
 
 from gdq.events.bsg import BSGTracker
 from gdq.runners.gdq import Runner as GDQRunner
@@ -8,13 +7,19 @@ from gdq.runners.gdq import Runner as GDQRunner
 
 class Runner(GDQRunner):
     def get_marathon(self) -> BSGTracker:
-        try:
-            return BSGTracker(
-                url=self.event_config["url"],
-                event_name=self.event_config["event"],
-            )
-        except KeyError as exc:
-            raise KeyError(f"`{exc!s}` key missing from configuration")
+        if "url" not in self.event_config:
+            raise KeyError("`url` key missing from configuration")
+
+        record_offsets = {}
+        if "offsets" in self.event_config:
+            record_offsets = self.event_config["offsets"]
+
+        return BSGTracker(
+            url=self.event_config["url"],
+            stream_index=-self.args.stream_index,
+            offset=self.args.delta_total,
+            record_offsets=record_offsets,
+        )
 
     def get_times(self) -> Tuple[datetime, Optional[datetime]]:
         event = self.get_marathon()
