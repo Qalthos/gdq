143d0debb523ccbcb570237d812dd57286bb5cc3
Kate Case <this.is@katherineca.se>
2022-11-21 11:13:46 -0500

    Fully purge bus from gdq

--- gdq/runners/__init__.py
+++ gdq/runners/__init__.py
@@ -2,32 +2,42 @@ import argparse
 import sys
 from typing import Optional
 
-from gdq.runners import bus, gdq, horaro
+from gdq.runners import gdq, horaro
 from gdq.runners.base import RunnerBase
 
 
 def get_base_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-n", "--interval", type=int, default=60,
+        "-n",
+        "--interval",
+        type=int,
+        default=60,
         help="time between screen refreshes",
     )
     parser.add_argument(
-        "--oneshot", action="store_true",
+        "--oneshot",
+        action="store_true",
         help="Run only once and then exit",
     )
     parser.add_argument(
-        "--list", action="store_true",
+        "--list",
+        action="store_true",
         help="List all known events instead of tracking one",
     )
     parser.add_argument(
-        "stream_name", nargs="?", type=str, default="gdq",
+        "stream_name",
+        nargs="?",
+        type=str,
+        default="gdq",
         help="The event to follow",
     )
     return parser
 
 
-def get_runner(config: dict[str, str], event_args: Optional[list[str]] = None) -> RunnerBase:
+def get_runner(
+    config: dict[str, str], event_args: Optional[list[str]] = None
+) -> RunnerBase:
     if event_args is None:
         event_args = []
 
@@ -35,8 +45,6 @@ def get_runner(config: dict[str, str], event_args: Optional[list[str]] = None) -
     if handler is None:
         print("Marathon type not set in config")
         sys.exit(1)
-    if handler == "bus":
-        return bus.Runner(config, event_args)
     if handler == "gdq":
         return gdq.Runner(config, event_args)
     if handler == "horaro":
